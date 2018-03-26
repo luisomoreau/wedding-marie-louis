a302a0a19d69c8d622149ce0997caae28ecf3b33
Louis Moreau <louis.moreau@sigfox.com>
2018-03-26 10:14:15 +0200

    Changed date on JS countdown

--- dist/scripts.js
+++ dist/scripts.js
@@ -905,7 +905,7 @@ google.maps.event.addDomListener(window, 'load', init);
 	};
 
 	// Set the date we're counting down to
-	var countDownDate = new Date("Dec 28, 2017 15:37:25").getTime();
+	var countDownDate = new Date("Oct 6, 2018 14:00:00").getTime();
 
 	// Update the count down every 1 second
 	var x = setInterval(function() {
